827af674ec62e1162a7e869a8a6e37810cf4b0a5
juvham <juvham@foxmail.com>
2016-01-29 10:52:07 +0800

    Initial Commit

---
+++ Kaiyan-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kaiyan-Swift
+//
+//  Created by 迅牛 on 16/1/29.
+//  Copyright © 2016年 juvham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
